75e39075252c53944b0d81090a12561e6bb050ab
Howie C <howie@pro.local>
2019-06-26 16:15:32 -0400

    Initial Commit

---
+++ SwiftFeedTests/SwiftFeedTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftFeedTests.swift
+//  SwiftFeedTests
+//
+//  Created by Howie C on 6/26/19.
+//  Copyright © 2019 Howie C. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftFeed
+
+class SwiftFeedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
